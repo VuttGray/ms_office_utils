bb939038d0d1f2d8f97683b1552b7b34e349689b
VuttGray <52618147+VuttGray@users.noreply.github.com>
2026-06-01 10:41:41 +0300

    Add get_account_folder by name in outlook_wrapper

--- ms_office_utils/outlook_wrapper.py
+++ ms_office_utils/outlook_wrapper.py
@@ -67,6 +67,9 @@ class OutlookWrapper:
                     counters[folder.Name + "/" + key] = sub_counters[key]
         return counters
 
+    def get_account_folder(self, account_name):
+        return self.__outlook.Folders(account_name)
+    
     def sent_items_counter(self, date):
         count = 0
         sent_items = self.__sent_items.Items
